79a12650a00eaf1dd0d895588bb4fce756e7b42b
ParkJuneWoo <korca02220@naver.com>
2021-10-14 15:10:32 +0900

    스크린 파일 생성(기본 파일 )  https://github.com/ifplant/ifplant_app/issues/1

---
+++ lib/app/ui/theme/app_text_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:ifplant_app/app/ui/theme/app_color.dart';
+
+final TextStyle baseTextStyle = TextStyle(
+  color: primaryColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
