1e7fe77921b77ec8801408567ccf44fe77dff102
Junewoo <korca02220@naver.com>
2021-10-19 22:40:00 +0900

    Home <-> Root 연결 https://github.com/ifplant/ifplant_app/issues/1

--- lib/app/root.dart
+++ lib/app/root.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'ui/android/screens.dart' show Home;
 
 class Root extends StatelessWidget {
   const Root({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO : BottomNavigation
-    return Scaffold(
-      body: Container(),
-    );
+    // TODO : 조건부 이동( 필요시 추가 )
+    return const Home();
   }
 }
