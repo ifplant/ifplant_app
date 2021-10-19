a983509382cc145d4f833e6bd1770a9a8a2aa6ed
kimiszero <zerokim0215@gmail.com>
2021-10-20 00:54:29 +0900

    3

--- lib/app/ui/android/home/home.dart
+++ lib/app/ui/android/home/home.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:ifplant_app/app/ui/theme/app_text_theme.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  Widget head() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(width: 30),
+          Text(
+            '내공간',
+            style: baseTextStyle,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: GestureDetector(onTap: () {}, child: const Text('완료')),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget myspace() {
+    return Container(
+      height: 500,
+      color: Colors.amber,
+    );
+  }
+
+  Widget myplant() {
+    return Container(
+      height: 200,
+      color: Colors.green,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            head(),
+            myspace(),
+            myplant(),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/app/ui/theme/app_text_theme.dart
+++ lib/app/ui/theme/app_text_theme.dart
@@ -4,5 +4,5 @@ import 'package:ifplant_app/app/ui/theme/app_color.dart';
 final TextStyle baseTextStyle = TextStyle(
   color: primaryColor,
   fontSize: 18,
-  fontWeight: FontWeight.w600,
+  fontWeight: FontWeight.w900,
 );
