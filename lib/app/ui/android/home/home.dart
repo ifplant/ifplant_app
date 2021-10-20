69d3121e0ae81d39a5dd7d29551188cb04527bee
kimeunyeong <zerokim0215@gmail.com>
2021-10-20 12:06:56 +0900

    test commit

--- lib/app/ui/android/home/home.dart
+++ lib/app/ui/android/home/home.dart
@@ -34,8 +34,23 @@ class Home extends StatelessWidget {
   Widget myplant() {
     return Container(
       height: 200,
+      width: 500,
       color: Colors.green,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _button(),
+        ],
+      ),
     );
+  }
+
+  Widget _button() {
+    return ElevatedButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add_circle,
+        ));
   }
 
   @override
