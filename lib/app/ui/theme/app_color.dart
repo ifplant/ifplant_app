89ecc6d11d70fc5c2efd03963582ffafcc140fa9
Junewoo <korca02220@naver.com>
2021-10-27 22:20:43 +0900

    컬러시트 추가 https://github.com/ifplant/ifplant_app/issues/1

--- lib/app/ui/theme/app_color.dart
+++ lib/app/ui/theme/app_color.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
-final Color primaryColor = Colors.green;
+const Color primaryColor = Color(0xFF186654);
+const Color secondaryColor = Color(0xFF57C8AE);
+const Color backgroundColor = Color(0xFFF3EFEA);
+const Color fontBaseColor = Color(0xFF2B2B2B);
